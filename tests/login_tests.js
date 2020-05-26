79f74688785670ef112169bc7a2ece40d1ef94a8
alexandru9027 <alexandru.dublea@gmail.com>
2020-05-26 11:05:31 +0300

    Remove unncessary timeout

--- tests/login_tests.js
+++ tests/login_tests.js
@@ -2,7 +2,7 @@ var loginPage=require('../pages/loginPage');
 describe ('Test login functionality', function() {
     it ('insert username and password and then login', async function() {
          await loginPage.get();
-         browser.sleep(2000);
+        
          await loginPage.enterUsername('guest@example.com');
          await loginPage.enterPassword('Password');
          await loginPage.clickLogin();
